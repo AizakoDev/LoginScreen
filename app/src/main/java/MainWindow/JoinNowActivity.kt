c5d8d9013a17f7ab61e9db966be036b7cfbe6fcf
AizakoDev <Aizako96@gmail.com>
2022-04-07 18:53:17 +0300

    created two empty windows

---
+++ app/src/main/java/MainWindow/JoinNowActivity.kt
@@ -0,0 +1,11 @@
+package MainWindow
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class JoinNowActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_join_now)
+    }
+}
